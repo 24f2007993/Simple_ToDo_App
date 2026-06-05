02d3a19b3f10f846fda3df219fd3a82f85ac1508
20f20-pixel <24f2007993@ds.study.iitm.ac.in>
2026-06-05 22:43:15 +0530

    First commit app.py

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Simple_ToDo_App
+Today I just created app.py
---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirecet, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toDoApp.db'
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug = True)
